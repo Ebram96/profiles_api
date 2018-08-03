8b201530c768c30d0b289feb9dda851a6c64f7c8
Ebram Shehata <ebram96@gmail.com>
2018-08-03 10:29:45 +0200

    Add .pyc files to .gitignore. Create UserProfile model and its manager. Set the user model in settings.py. Create migrations and migrate.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """The model manager for UserProfile model"""
+    def create_user(self, email, first_name, last_name, password=None):
+        """Creates a new UserProfile object"""
+        if not email:
+            raise ValueError("An email address must be provided!")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        """Creates and saves a super user"""
+        user = self.create_user(email, first_name, last_name, password)
+
+        user.is_active = user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represents a user's profile"""
+    email      = models.EmailField(max_length=256, unique=True, null=False)
+    first_name = models.CharField(max_length=256)
+    last_name  = models.CharField(max_length=256)
+    is_active  = models.BooleanField(default=True)
+    is_staff   = models.BooleanField(default=False)
+
+    objects    = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        """Returns user's full name"""
+        return self.first_name + " " + self.last_name
+
